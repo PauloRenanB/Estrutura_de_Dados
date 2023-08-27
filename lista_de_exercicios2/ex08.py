015b2dc3f426a7f32084acbebc28398e13934e7f
PauloRenanB <paulorenanb@hotmail.com>
2023-08-27 17:04:30 -0300

    Adicionado estrutura condicional em metodo

--- lista_de_exercicios2/ex08.py
+++ lista_de_exercicios2/ex08.py
@@ -4,8 +4,12 @@
 class Aluno:
     def calcular_media(self, nome, nota1, nota2, nota3):
         media = (nota1 + nota2 + nota3) / 3
-        print(f"Media: {media}. Aluno: {nome}")
+        if media >= 7:
+            print(f"Media: {media:.2f}. Aluno: {nome}")
+        else:
+            print("Reprovado")
+        
         return media
     
 aluno1 = Aluno()
-aluno1.calcular_media("Paulo Renan", 10, 7, 7)
+aluno1.calcular_media("Paulo Renan", 10, 9, 6)
